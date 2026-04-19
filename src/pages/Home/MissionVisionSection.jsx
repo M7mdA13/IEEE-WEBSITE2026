dc5356b545f65fe6e8e3c6f3ce5c1dd8bc0b9dfb
m7mda13 <madham20177@gmail.com>
2026-04-19 22:22:14 +0200

    accessibility & performance tweaks

--- src/pages/Home/MissionVisionSection.jsx
+++ src/pages/Home/MissionVisionSection.jsx
@@ -93,7 +93,7 @@ const MissionVisionSection = () => {
         </div>
 
         <div className="mv-award-banner">
-          <span className="mv-award-trophy" aria-label="trophy">
+          <span className="mv-award-trophy" role="img" aria-label="trophy">
             <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
               {/* Cup body */}
               <path d="M8 4h16v12a8 8 0 0 1-16 0V4Z" fill="url(#trophyGold)" stroke="#b8860b" strokeWidth="1"/>
