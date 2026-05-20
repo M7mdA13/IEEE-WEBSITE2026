8eb0cac940a1820405ba7f57db1b3a637646fdf1
Mohamed Adham <125884839+M7mdA13@users.noreply.github.com>
2026-05-20 13:05:06 +0300

    Change server listen address to '0.0.0.0'

--- server.js
+++ server.js
@@ -54,6 +54,6 @@ app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT} [${process.env.NODE_ENV || 'development'}]`);
 });
